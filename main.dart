4944dc625876ccdf61cca0125ad41f377395ae04
Rehman Ali <rehmanali.pk60@gmail.com>
2025-11-17 10:34:35 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main(){
+  int x = 80;
+  // prefix, postfixt
+  ++x; //prefix increament
+  --x; // prefix decremeant
+  x++; // postfix increament
+  x--; //postfixt decrement
+  print(x);
+
+}
